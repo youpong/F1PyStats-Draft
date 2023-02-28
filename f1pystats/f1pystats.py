0f8954491ed4204dd2b8dbe63ee8725b4598a936
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-02-28 21:41:34 +0900

    Introduce driver_id

--- f1pystats/f1pystats.py
+++ f1pystats/f1pystats.py
@@ -6,3 +6,30 @@ def pit_stops(year: int, race_round: int, stop_number: int = 0, fastest: bool = 
     args['fastest'] = fastest
 
     return args
+
+def pit_stopsA(year: int, race_round: int, driver_id: str=None, stop_number: int = 0, fastest: bool = False):
+    args = {}
+    args['year'] = year
+    args['race_round'] = race_round
+    args['stop_number'] = stop_number
+    args['fastest'] = fastest
+
+    return args
+
+def pit_stopsB(year: int, race_round: int, stop_number: int = 0, fastest: bool = False, driver_id: str=None):
+    args = {}
+    args['year'] = year
+    args['race_round'] = race_round
+    args['stop_number'] = stop_number
+    args['fastest'] = fastest
+
+    return args
+
+def pit_stopsC(year: int, race_round: int, *, stop_number: int = 0, fastest: bool = False, driver_id: str=None):
+    args = {}
+    args['year'] = year
+    args['race_round'] = race_round
+    args['stop_number'] = stop_number
+    args['fastest'] = fastest
+
+    return args
--- tests/test_pit_stops.py
+++ tests/test_pit_stops.py
@@ -1,9 +1,30 @@
 class TestPitStops:
     import f1pystats.f1pystats as fp
 
-    def test_pit_stops_race(self):
-        args = self.fp.pit_stops(2023, 24)
+    def test_pit_stops(self):
+        args = self.fp.pit_stops(2023, 24, 1)
         assert args['year'] == 2023
         assert args['race_round'] == 24
-        assert args['stop_number'] == 0
+        assert args['stop_number'] == 1
+        assert args['fastest'] == False
+
+    def test_pit_stopsA(self):
+        args = self.fp.pit_stopsA(2023, 24, 1)
+        assert args['year'] == 2023
+        assert args['race_round'] == 24
+        assert args['stop_number'] == 1 # fail
+        assert args['fastest'] == False
+
+    def test_pit_stopsB(self):
+        args = self.fp.pit_stopsB(2023, 24, 1)
+        assert args['year'] == 2023
+        assert args['race_round'] == 24
+        assert args['stop_number'] == 1
+        assert args['fastest'] == False
+
+    def test_pit_stopsC(self):
+        args = self.fp.pit_stopsC(2023, 24, stop_number=1)
+        assert args['year'] == 2023
+        assert args['race_round'] == 24
+        assert args['stop_number'] == 1
         assert args['fastest'] == False
